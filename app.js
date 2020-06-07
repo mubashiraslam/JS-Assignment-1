33bef96a7b247d50a448419d225ec9b0e29c78b8
Mubashir <mavayani2@gmail.com>
2020-06-07 16:48:29 +0500

    Completed

---
+++ app.js
@@ -0,0 +1,64 @@
+// Chapter 1 Task #1
+
+alert("Good Morning!");
+
+
+// Chapter 1 Task #2
+
+alert("Error! Please enter a valid password.");
+
+
+// Chapter 1 Task #3
+
+alert("Welcome to JS land... \n Happy Coding!");
+
+
+// Chapter 1 Task #4
+
+alert("Welcome to JS land...");
+alert("Happy Coding!");
+
+
+// Chapter 1 Task #5
+
+console.log("Hello... I can run JS through my web browser's console");
+
+
+// Chapter 1 Task #6
+
+alert("Hey, I am using JS alert in my HTML Project");
+
+
+// Chapter 2 Task #1
+
+var username;
+
+
+// Chapter 2 Task #2
+
+var myName;
+myName = "Muhammad Mubashir Aslam";
+
+
+// Chapter 2 Task #3
+
+var message;
+message = "Hello World";
+alert(message);
+
+
+// Chapter 2 Task #4
+
+var name, age, qualification;
+name = prompt("Enter your Name");
+age = prompt("Enter your age","example 18");
+qualification = prompt("Enter your Qualification");
+alert(name);
+alert(age);
+alert(qualification);
+
+
+// Chapter 2 Task #5
+
+var pizza = "PIZZA";
+alert(pizza + "\n" + pizza + "\b");
